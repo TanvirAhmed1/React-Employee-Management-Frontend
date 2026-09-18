ffb9b679095493ce3229ca5920c83a2d46220c6c
TanvirAhmed1 <tanvirnoor38@yahoo.com>
2026-09-18 23:58:32 +0600

    React Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import './App.css'
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
 
 
   return (
     <>
+    <Router>
       <HeaderComponent />
-      <ListEmployeeComponent />
+      <Routes>
+        <Route path="/" element={<ListEmployeeComponent />} />
+        <Route path="/employees" element={<ListEmployeeComponent />} />
+      </Routes>
       <FooterComponent />
+    </Router>
     </>
   )
 }
